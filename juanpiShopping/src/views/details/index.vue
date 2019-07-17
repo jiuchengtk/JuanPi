ab8b0a4bc0b24a6d270b491e6b03601e6ccce66d
crzscf <1335363505@qq.com>
2019-07-17 19:41:53 +0800

    17-2

--- juanpiShopping/src/views/details/index.vue
+++ juanpiShopping/src/views/details/index.vue
@@ -48,7 +48,7 @@ export default {
       goodsId: '',
       quota: 0,
       quotaUsed: 0,
-      closeOnClickOverlay: false,
+      closeOnClickOverlay: true,
       sku: {
         // 所有sku规格类目与其值的从属关系，比如商品有颜色和尺码两大类规格，颜色下面又有红色和蓝色两个规格值。
         // 可以理解为一个商品可以有多个规格类目，一个规格类目下可以有多个规格值。
@@ -79,8 +79,30 @@ export default {
               {
                 id: '1111',
                 name: 'xxl'
+              },
+              {
+                id: '1222',
+                name: 'xxxl'
               }
-            ],
+            ]
+            // k_s: 's1' // skuKeyStr：sku 组合列表（下方 list）中当前类目对应的 key 值，value 值会是从属于当前类目的一个规格值 id
+          },
+          {
+            k: 'bb', // skuKeyName：规格类目名称
+            v: [
+              {
+                id: '1000', // skuValueId：规格值 id
+                name: 'xl' // skuValueName：规格值名称
+              },
+              {
+                id: '1111',
+                name: 'xxl'
+              },
+              {
+                id: '1222',
+                name: 'xxxl'
+              }
+            ]
             // k_s: 's1' // skuKeyStr：sku 组合列表（下方 list）中当前类目对应的 key 值，value 值会是从属于当前类目的一个规格值 id
           }
         ],
@@ -89,6 +111,14 @@ export default {
             // id: 2259, // skuId，下单时后端需要
             price: 100, // 价格（单位分）
             s1: '1215', // 规格类目 k_s 为 s1 的对应规格值 id
+            s2: '1193', // 规格类目 k_s 为 s2 的对应规格值 id
+            s3: '0', // 最多包含3个规格值，为0表示不存在该规格
+            stock_num: 110 // 当前 sku 组合对应的库存
+          },
+          {
+            // id: 2259, // skuId，下单时后端需要
+            price: 100, // 价格（单位分）
+            s1: '30349', // 规格类目 k_s 为 s1 的对应规格值 id
             s2: '1193', // 规格类目 k_s 为 s2 的对应规格值 id
             s3: '0', // 最多包含3个规格值，为0表示不存在该规格
             stock_num: 110 // 当前 sku 组合对应的库存
@@ -101,7 +131,7 @@ export default {
       },
       goods: {
         // 商品标题
-        // title: '测试商品',
+        title: '商品title' // 商品名称
         // 默认商品 sku 缩略图
         // picture: 'https://img.yzcdn.cn/1.jpg'
       }
@@ -121,11 +151,9 @@ export default {
       console.log('onadd')
     },
     buy () {
-      console.log(this.show)
       if (this.show === false) {
         this.show = true
       }
-      // this.show = !this.show
     },
     close () {
       console.log('close')
@@ -146,6 +174,8 @@ export default {
       .then(res => res.json()).then(data => {
         this.title = data[0].title
         this.id = data[0].id
+        // console.log(this.goods.title)
+        this.goods.title = data[0].title
       })
   }
 }
